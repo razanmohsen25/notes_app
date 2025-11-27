8c0deb56ab22b14b887be6158f6b1ffdd1cb00db
my-company-dev-admin <razanmohsen618@gmail.com>
2025-11-27 11:07:31 +0200

    change theme and create notes view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class notesView extends StatelessWidget{
+  const notesView ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold();
+  }
+}
