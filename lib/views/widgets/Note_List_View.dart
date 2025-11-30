3a2dbbba93e1b85b5c0de0ea650d2a8a313fa5df
my-company-dev-admin <razanmohsen618@gmail.com>
2025-11-30 11:30:43 +0200

    create Note List View

---
+++ lib/views/widgets/Note_List_View.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/cupertino.dart';
+
+import 'Custom_note_item.dart';
+
+class NoteListView extends StatelessWidget{
+  const NoteListView ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context,index)
+    {
+      return const Padding(
+        padding:  EdgeInsets.symmetric(vertical:8),
+        child:  NoteItem(),
+      );
+    });
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'Custom_app_Bar.dart';
 import 'Custom_note_item.dart';
+import 'Note_List_View.dart';
 
 class NotesViewBody extends StatelessWidget{
   const NotesViewBody ({super. key}) ;
@@ -15,24 +16,13 @@ class NotesViewBody extends StatelessWidget{
         children: const[
           SizedBox(height: 50,),
           CustomAppBar(),
-          NoteItem(),
+
+          Expanded(child: NoteListView()),
 
 
 
         ],
       ),
     );
-  }
-}
-
-class NoteListView extends StatelessWidget{
-  const NoteListView ({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context,index)
-    {
-      return const NoteItem();
-    });
   }
 }
