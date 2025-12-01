2ca529d904846b18739c5618e4792e80c6808729
my-company-dev-admin <razanmohsen618@gmail.com>
2025-12-01 16:42:16 +0200

    Provider add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes/Models/Note_model.dart';
+import 'package:notes/cubits/Add_note-cubit/add_note_cubit.dart';
 import 'package:notes/views/notes_view.dart';
 import 'package:notes/views/widgets/constants.dart';
 
@@ -17,12 +19,17 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,
-      fontFamily: "Poppins",
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark,
+        fontFamily: "Poppins",
+        ),
+        home:const notesView(),
       ),
-      home:const notesView(),
     );
   }
 
